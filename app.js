6505e8b0e1375a6a2bd0411eeaba477dd87f023c
JoseMandujanoCarrillo <mandujanocarrillo27@gmail.com>
2025-02-26 09:24:51 -0600

    Pagina

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors'); // Se importa el paquete CORS
 const setupSwagger = require('./config/swagger');
 const cleanerRoutes = require('./routes/cleanerRoutes');
 const proposalRoutes = require('./routes/proposalRoutes');
@@ -12,6 +13,13 @@ const app = express();
 // Middleware
 app.use(express.json());
 
+// Configuración de CORS: Puedes ajustar 'origin' según tus necesidades
+app.use(cors({
+  origin: 'http://127.0.0.1:5500', // Permite solicitudes desde este origen. Si deseas permitir todos los orígenes, puedes usar '*'
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 // Swagger
 setupSwagger(app);
 
@@ -22,6 +30,7 @@ app.use('/services', serviceRoutes);
 app.use('/users', userRoutes);
 app.use('/creditcards', creditCardRoutes); // Aquí se define la ruta base
 app.use('/auditors', auditorRoutes);
+
 // Iniciar el servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
